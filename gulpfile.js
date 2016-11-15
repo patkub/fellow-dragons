9b7f39e97ace08c9a8115ac2f6ce02919f121db7
Patrick Kubiak <patrick@antuple.net>
2016-11-14 22:57:28 -0500

    minify vulcanized output

--- gulpfile.js
+++ gulpfile.js
@@ -23,48 +23,54 @@ gulp.task('minify-html', function() {
 // Compile LESS files from /less into /css
 gulp.task('less', function() {
     return gulp.src('less/clean-blog.less')
-        .pipe(less())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('css'));
+      .pipe(less())
+      .pipe(rename({ suffix: '.min' }))
+      .pipe(gulp.dest('css'));
 });
 
 // Minify compiled CSS
 gulp.task('minify-css', ['less'], function() {
     return gulp.src('css/clean-blog.css')
-        .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('css'));
+      .pipe(cleanCSS({ compatibility: 'ie8' }))
+      .pipe(rename({ suffix: '.min' }))
+      .pipe(gulp.dest('css'));
 });
 
 // Minify JS
 gulp.task('minify-js', function() {
     return gulp.src('js/clean-blog.js')
-        .pipe(uglify())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('js'));
+      .pipe(uglify())
+      .pipe(rename({ suffix: '.min' }))
+      .pipe(gulp.dest('js'));
 });
 
 // Vulcanize Web Components
 gulp.task('vulcanize', function () {
     gulp.src('src/components.html')
-        .pipe(vulcanize({
-            stripComments: true,
-            inlineScripts: true,
-            inlineCss: true
-        }))
-        .pipe(replace('../node_modules/font-awesome/fonts/', 'fonts/'))
-        .pipe(rename('critical.html'))
-        .pipe(gulp.dest('dist'));
+      .pipe(vulcanize({
+        stripComments: true,
+        inlineScripts: true,
+        inlineCss: true
+      }))
+      .pipe(htmlmin({
+        collapseWhitespace: true,
+        removeComments: true,
+        minifyCSS: true,
+        minifyJS: true
+      }))
+      .pipe(replace('../node_modules/font-awesome/fonts/', 'fonts/'))
+      .pipe(rename('critical.html'))
+      .pipe(gulp.dest('dist'));
 });
 
 // Copy files
 gulp.task('copy', function() {
     gulp.src(['img/**'])
-        .pipe(gulp.dest('dist/img'))
+      .pipe(gulp.dest('dist/img'))
     gulp.src(['node_modules/font-awesome/fonts/**'])
-        .pipe(gulp.dest('dist/fonts'))
+      .pipe(gulp.dest('dist/fonts'))
     gulp.src(['bower_components/webcomponentsjs/webcomponents-lite.min.js'])
-        .pipe(gulp.dest('dist/js'));
+      .pipe(gulp.dest('dist/js'));
 })
 
 // Run everything
