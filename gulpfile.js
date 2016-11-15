2c79326d22f70d6f23e1dc715bc5e3f32340a16a
Patrick Kubiak <patrick@antuple.net>
2016-11-14 22:45:53 -0500

    woot pagespeed 87 mobile, 93 desktop

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,6 @@ var replace = require('gulp-replace');
 var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
 var vulcanize = require('gulp-vulcanize');
-var pkg = require('./package.json');
-
-// Compile LESS files from /less into /css
-gulp.task('less', function() {
-    return gulp.src('less/clean-blog.less')
-        .pipe(less())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('dist/css'))
-});
 
 // Minify HTML
 gulp.task('minify-html', function() {
@@ -29,12 +20,20 @@ gulp.task('minify-html', function() {
     .pipe(gulp.dest('dist'));
 });
 
+// Compile LESS files from /less into /css
+gulp.task('less', function() {
+    return gulp.src('less/clean-blog.less')
+        .pipe(less())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('css'));
+});
+
 // Minify compiled CSS
 gulp.task('minify-css', ['less'], function() {
     return gulp.src('css/clean-blog.css')
         .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('dist/css'))
+        .pipe(gulp.dest('css'));
 });
 
 // Minify JS
@@ -42,45 +41,31 @@ gulp.task('minify-js', function() {
     return gulp.src('js/clean-blog.js')
         .pipe(uglify())
         .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('dist/js'))
+        .pipe(gulp.dest('js'));
 });
 
 // Vulcanize Web Components
 gulp.task('vulcanize', function () {
     gulp.src('src/components.html')
-      .pipe(vulcanize({
-          stripComments: true,
-          inlineScripts: true,
-          inlineCss: true
-      }))
-      .pipe(rename('critical.html'))
-      .pipe(gulp.dest('dist'));
+        .pipe(vulcanize({
+            stripComments: true,
+            inlineScripts: true,
+            inlineCss: true
+        }))
+        .pipe(replace('../node_modules/font-awesome/fonts/', 'fonts/'))
+        .pipe(rename('critical.html'))
+        .pipe(gulp.dest('dist'));
 });
 
-// Copy vendor libraries from /node_modules into /vendor
+// Copy files
 gulp.task('copy', function() {
-    gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('dist/vendor/bootstrap'))
-
-    gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('dist/vendor/jquery'))
-
-    gulp.src([
-            'node_modules/font-awesome/**',
-            '!node_modules/font-awesome/**/*.map',
-            '!node_modules/font-awesome/.npmignore',
-            '!node_modules/font-awesome/*.txt',
-            '!node_modules/font-awesome/*.md',
-            '!node_modules/font-awesome/*.json'
-        ])
-        .pipe(gulp.dest('dist/vendor/font-awesome'))
-
     gulp.src(['img/**'])
         .pipe(gulp.dest('dist/img'))
-
+    gulp.src(['node_modules/font-awesome/fonts/**'])
+        .pipe(gulp.dest('dist/fonts'))
     gulp.src(['bower_components/webcomponentsjs/webcomponents-lite.min.js'])
-        .pipe(gulp.dest('dist/js'))
+        .pipe(gulp.dest('dist/js'));
 })
 
 // Run everything
-gulp.task('default', ['less', 'minify-html', 'minify-css', 'minify-js', 'vulcanize', 'copy']);
+gulp.task('default', ['less', 'minify-css', 'minify-html', 'minify-js', 'vulcanize', 'copy']);
