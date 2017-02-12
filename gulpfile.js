6fc1d59f1141be25e94523e16a24d0faa3f175f2
Patrick Kubiak <patrick@antuple.net>
2017-02-11 19:25:18 -0500

    fixed less/css paths

--- gulpfile.js
+++ gulpfile.js
@@ -33,7 +33,7 @@ gulp.task('less', function() {
 
 // Minify compiled CSS
 gulp.task('minify-css', ['less'], function() {
-    return gulp.src('app/css/clean-blog.css')
+    return gulp.src('css/clean-blog.css')
       .pipe(cleanCSS({ compatibility: 'ie8' }))
       .pipe(rename({ suffix: '.min' }))
       .pipe(gulp.dest('css'));
